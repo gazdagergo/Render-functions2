09450e5b130e495390fdfe709fd72aa653418366
gazdagergo <info@gazdagergo.com>
2018-01-25 16:27:01 +0100

    w/o render func

--- src/confirmationWrapper.jsx
+++ src/confirmationWrapper.jsx
@@ -1,21 +1,50 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 
-class ConfirmationWrapper extends React.Component {
+class ConfirmationModal extends React.Component {
 
   state = {
     modalVisible: false
   };
 
   render(){
-    return (
+    if (this.state.modalVisible) {
+      return (
       <div>
-        {this.props.children({
-          ...this.state,
-          ...this.props
-        })}
+        <div style={{
+          background: 'gray',
+          display: 'inline-block',
+          position: 'absolute',
+          padding: 12,
+          borderRadius: 5,
+          left: 54,
+          top: 16
+        }}>
+          {this.props.text}<br />
+          <button
+            onClick={() => {
+              this.props.onConfirm()
+              this.setState({modalVisible: false})
+            }}
+          >
+            Yes
+          </button>
+          <button
+            onClick={ () => this.setState({ modalVisible: false }) }
+          >
+            No
+          </button>
+        </div>
+        {this.props.children}
+      </div>
+      )   
+    }
+
+    return (
+      <div onClick={() => this.setState({ modalVisible: true })}>
+        {this.props.children}
       </div>
     )
   }
 };
 
-export default ConfirmationWrapper;
+export default ConfirmationModal;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
-import ConfirmationWrapper from './confirmationWrapper';
+import ConfirmationModal from './confirmationWrapper';
 import articleHelper from './articleHelper';
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <ConfirmationWrapper>
-          {
-            ({modalVisible}) => {
-              if (modalVisible) {
-                return (
-                  <div>
-                    Delete article?<br />
-                    <button
-                      onClick={() => articleHelper.deleteArticle()}>
-                      Yes
-                    </button>
-                    <button>
-                      No
-                    </button>
-                  </div>
-                )
-              }
-              return <button>Delete</button>;
-            }
-          }
-        </ConfirmationWrapper>
+        <ConfirmationModal
+          text="Delete this article?"
+          onConfirm={ () => articleHelper.deleteArticle() }
+        >
+          <button>Delete</button>
+        </ConfirmationModal>
       </div>
     )
   }
